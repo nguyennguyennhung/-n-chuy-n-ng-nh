fb373675b205a6d9aee6767878c2f773b2390986
benhv1en <184822003+benhv1en@users.noreply.github.com>
2026-05-05 16:46:10 +0700

    commit cuối ngày như bi đầu ngày
    liệu mà đọc mà pull về

--- Assets/Scripts/ShapeSceneLoader.cs
+++ Assets/Scripts/ShapeSceneLoader.cs
@@ -3,23 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class ShapeSceneLoader : MonoBehaviour
 {
-    [SerializeField] private string targetSceneName;
+    [SerializeField]
+    private string targetSceneName;
 
-    // Hàm này sẽ được gọi từ Button OnClick()
     public void LoadTargetScene()
     {
         if (string.IsNullOrWhiteSpace(targetSceneName))
         {
             Debug.LogError($"[{name}] targetSceneName đang rỗng.");
-            return   ;
+            return;
         }
 
         SceneManager.LoadSceneAsync(targetSceneName);
-    }
-
-    // Dùng nếu mày muốn set scene bằng code từ chỗ khác
-    public void SetTargetScene(string sceneName)
-    {
-        targetSceneName = sceneName;
     }
 }
